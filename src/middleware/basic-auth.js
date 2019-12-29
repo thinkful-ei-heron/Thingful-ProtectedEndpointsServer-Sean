4883e15d6cdd65d6535bc6e8d76e59b8f5df5419
seank329 <seankamran@gmail.com>
2019-12-28 23:12:50 -0500

    Jwt Implementation

--- src/middleware/basic-auth.js
+++ src/middleware/basic-auth.js
@@ -1,4 +1,5 @@
-const bcrypt = require('bcryptjs')
+//const bcrypt = require('bcryptjs')
+const AuthService = require('../auth/auth-service')
 
 function requireAuth(req, res, next){
     // console.log('requireAuth')
@@ -11,25 +12,30 @@ function requireAuth(req, res, next){
     } else {
         basicToken = authToken.slice('basic '.length, authToken.length)
     }
-    const [tokenUserName, tokenPassword] = Buffer
-        .from(basicToken, 'base64')
-        .toString()
-        .split(':')
+    const [tokenUserName, tokenPassword] = AuthService.parseBasicToken(basicToken)
+        // .from(basicToken, 'base64')
+        // .toString()
+        // .split(':')
     
     if(!tokenUserName || !tokenPassword) {
         return res.status(401).json({error : 'Unauthorized request'})
     }
 
-    req.app.get('db')('thingful_users')
-        .where({ user_name : tokenUserName })
-        .first()
+    // req.app.get('db')('thingful_users')
+    //     .where({ user_name : tokenUserName })
+    //     .first()
+    AuthService.getUserWithUserName(
+        req.app.get('db'),
+        tokenUserName
+    )
         .then(user => {
             if(!user) {
                 return res.status(401).json({error: 'Unauthorized request' })
             }
             // req.user = user
             // next()
-            return bcrypt.compare(tokenPassword, user.password)
+            //return bcrypt.compare(tokenPassword, user.password)
+              return AuthService.comparePasswords(tokenPassword, user.password)
                 .then(passwordMatch => {
                     if(!passwordMatch) {
                         return res.status(401).json({ error: 'Unauthorized request' })
